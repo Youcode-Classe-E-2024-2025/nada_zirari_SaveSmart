4cb5691e7985e303a884d43d6a306c9377ef70d9
Nada Zirari <zirarinada20@gmail.com>
2025-02-24 14:48:21 +0000

    add the necessary routes for login and regoster

--- savesmart/routes/web.php
+++ savesmart/routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
