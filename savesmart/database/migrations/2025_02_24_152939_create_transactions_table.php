6c3a04f64eed3bf5a88ffbdab62f07f432474a6f
Nada Zirari <zirarinada20@gmail.com>
2025-02-24 16:18:27 +0000

    add eloquant for transaction

--- savesmart/database/migrations/2025_02_24_152939_create_transactions_table.php
+++ savesmart/database/migrations/2025_02_24_152939_create_transactions_table.php
@@ -4,16 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('type'); // revenu ou dépense
+            $table->string('title');
+            $table->decimal('amount', 10, 2);
+            $table->date('date');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
         });
     }
 
